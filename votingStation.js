4377ad31bb7bbb30c2810e53c731182b2fd27a04
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-04-05 00:17:18 +0000

    check if buildings meet required specifications

---
+++ votingStation.js
@@ -0,0 +1,31 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+
+function chooseStations (stations) {
+  const acceptableOption = []
+  const unacceptableOption = []
+
+  for (const station of stations) {
+    let capacity = station[1]
+    let buildingName = station[0]
+    let buildingType = station[2]
+    
+    if (capacity >= 20) {
+      if (buildingType === 'community centre' || buildingType === 'school') {
+        acceptableOption.push(buildingName)
+      }
+    } else {
+      unacceptableOption.push(buildingName)
+    }
+    console.log(station)
+
+  }
+
+  return acceptableOption
+}
+
+console.log(chooseStations(stations));
