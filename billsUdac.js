72c5cda011350caa29013732af4a9c7adaa3dac4
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-04-06 00:51:25 +0000

    array lessions from js course

---
+++ billsUdac.js
@@ -0,0 +1,12 @@
+var bills = [50.23, 19.12, 34.01,
+  100.11, 12.15, 9.90, 29.11, 12.99,
+  10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+
+let totals = bills.map(function(elem) {
+  elem *= 1.15; elem = elem.toFixed(2); elem = Number(elem);
+  return elem
+});
+
+console.log(totals);
+
